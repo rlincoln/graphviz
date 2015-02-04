e2b9bf57e8ca83283e46f42b984e44a167ccbc9b
rwl <r.w.lincoln@gmail.com>
2015-02-04 18:02:51 +0100

    Moving ppapi_common to ppapi-common.

--- ppapi-common/build.sh
+++ ppapi-common/build.sh
@@ -7,10 +7,10 @@ ConfigureStep() {
 
 BuildStep() {
   SetupCrossEnvironment
-  cd ${START_DIR}/
+  LogExecute cd ${START_DIR}/
 #  export OS_JOBS=1
 
-  export TOOLCHAIN=pnacl
+  LogExecute export TOOLCHAIN=pnacl
 
   DefaultBuildStep
 }
@@ -21,7 +21,8 @@ TestStep() {
 
 InstallStep() {
   MakeDir ${DESTDIR_LIB}
-  LogExecute install ${START_DIR}/pnacl/Release/libppapi_common.a ${DESTDIR_LIB}/
+  #LogExecute install ${START_DIR}/pnacl/Release/libppapi_common.a ${DESTDIR_LIB}/
+  LogExecute install ${NACL_SDK_LIBDIR}/libppapi_common.a ${DESTDIR_LIB}/
   MakeDir ${DESTDIR_INCLUDE}
   LogExecute install ${START_DIR}/lib/ppapi_common.h ${DESTDIR_INCLUDE}/
   #DefaultInstallStep
